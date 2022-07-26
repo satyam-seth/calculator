5e09150b4a762234f0e52c0feca56c934c0e42a4
satyam-seth <satyam1998.1998@gmail.com>
2022-07-26 17:10:02 +0530

    add button click handler

--- src/ts/calculator.ts
+++ src/ts/calculator.ts
@@ -1,10 +1,9 @@
 export default class Calculator {
   // eslint-disable-next-line class-methods-use-this
-  private screenSkeleton(): HTMLElement {
-    const screen = document.createElement('div');
-    screen.classList.add('screen');
-    screen.innerText = '0';
-    return screen;
+  private displaySkeleton(): HTMLElement {
+    const display = document.createElement('div');
+    display.classList.add('display');
+    return display;
   }
 
   // eslint-disable-next-line class-methods-use-this
@@ -37,17 +36,62 @@ export default class Calculator {
     buttonTexts.forEach((text) => {
       const button = document.createElement('button');
       button.innerText = text;
+      button.addEventListener('click', this.handleButtonClick.bind(this));
       buttonsSkeleton.appendChild(button);
     });
 
     return buttonsSkeleton;
   }
 
+  private handleButtonClick(event: Event): void {
+    const button = event.target as HTMLButtonElement;
+
+    switch (button.innerText) {
+      case 'AC':
+        this.clearDisplay();
+        break;
+      case 'DEL':
+        this.deleteDisplay();
+        break;
+      case '=':
+        this.calculate();
+        break;
+      default:
+        this.updateDisplay(button.innerText);
+    }
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  private calculate(): void {
+    const display = document.querySelector('.display') as HTMLDivElement;
+    const expression = display.innerText;
+    // eslint-disable-next-line no-eval
+    const result = eval(expression) as number;
+    display.innerText = result.toString();
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  private deleteDisplay(): void {
+    const display = document.querySelector('.display') as HTMLDivElement;
+    display.innerText = display.innerText.slice(0, -1);
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  private clearDisplay(): void {
+    (document.querySelector('.display') as HTMLDivElement).innerText = '';
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  private updateDisplay(value: string): void {
+    const display = document.querySelector('.display') as HTMLDivElement;
+    display.innerText += value;
+  }
+
   private skeleton(): HTMLElement {
     const skeleton = document.createElement('div');
     skeleton.classList.add('calculator');
 
-    skeleton.append(this.screenSkeleton(), this.buttonsSkeleton());
+    skeleton.append(this.displaySkeleton(), this.buttonsSkeleton());
     return skeleton;
   }
 
